9bee6c88cbd8417caa48abe55ae3adb7aef7ee9d
vladka.dovzh@gmail.com <vladka.dovzh@gmail.com>
2020-09-23 13:15:02 +0400

    create template for list employers

--- popup.js
+++ popup.js
@@ -1,15 +1,19 @@
 document.addEventListener('DOMContentLoaded', function() {
   function getListContent() {
     let result = [];
+    let name = 'Андрей Инкерман';
+    let mail = 'inkerman@irlix.com';
   
     for(let i=1; i<=3; i++) {
       let li = document.createElement('li');
-      li.append(i);
+      li.className = 'list-group-item list-group-item-action';
+      li.innerHTML = `<div>${i}${name}</div><div>${mail}</div>`;
       result.push(li);
     }
   
     return result;
   }
   let ul = document.getElementById('list');
+  ul.append(...getListContent());
   
 }, false)
